110a1ceaedc01ef043ffa506e69166162ebd5e2a
cccccrius <crius38@gmail.com>
2025-03-24 14:04:54 +0100

    Create ubuntu_install_config.sh

---
+++ ubuntu_install_config.sh
@@ -0,0 +1,166 @@
+#change your firefox folder - here for ubuntu mozilla deb repo firefox
+DIR=/usr/lib/firefox
+
+sudo snap remove firefox
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt-get update && sudo apt-get install -y firefox firefox-l10n-fr --allow-downgrades
+
+sudo \rm $DIR/defaults/pref/autoconfig.js 2> /dev/null
+sudo mkdir -p $DIR/defaults/pref/ 2> /dev/null
+sudo tee $DIR/defaults/pref/autoconfig.js >/dev/null <<'EOF'
+//
+pref("general.config.sandbox_enabled", false);
+pref("general.config.filename", "firefox.cfg");
+pref("general.config.obscure_value", 0);
+EOF
+
+sudo \rm $DIR/firefox.cfg 2> /dev/null
+sudo tee $DIR/firefox.cfg >/dev/null <<'EOF'
+//the first line is always a comment
+pref("network.trr.mode", 3);
+pref("network.trr.uri", "https://dns.adguard.com/dns-query");
+pref("network.trr.custom_uri", "https://dns.adguard.com/dns-query");
+pref("network.trr.bootstrapAddress", "1.1.1.2");
+pref("network.trr.default_provider_uri", "https://dns.adguard.com/dns-query");
+pref("extensions.autoDisableScopes", 0);
+pref("browser.aboutConfig.showWarning", false);
+pref("browser.startup.homepage", "https://www.google.fr");
+pref("startup.homepage_welcome_url", "https://www.google.fr");
+pref("startup.homepage_override_url", "https://www.google.fr");
+pref("browser.shell.checkDefaultBrowser", false);
+pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+pref("app.update.silent", true);
+pref("browser.messaging-system.whatsNewPanel.enabled", false);
+pref("privacy.trackingprotection.enabled", true);
+pref("privacy.firstparty.isolate", true);
+pref("privacy.donottrackheader.enabled", true);
+pref("privacy.globalprivacycontrol.enabled", true);
+pref("privacy.globalprivacycontrol.functionality.enabled", true);
+pref("toolkit.telemetry.archive.enabled", false);
+pref("toolkit.telemetry.enabled", false);
+pref("toolkit.telemetry.rejected", true);
+pref("toolkit.telemetry.unified", false);
+pref("toolkit.telemetry.unifiedIsOptIn", false);
+pref("toolkit.telemetry.prompted", 2);
+pref("toolkit.telemetry.rejected", true);
+pref("datareporting.policy.dataSubmissionEnabled", false);
+pref("datareporting.healthreport.service.enabled", false);
+pref("datareporting.healthreport.uploadEnabled", false);
+pref("app.shield.optoutstudies.enabled", false);
+pref("browser.urlbar.suggest.pocket", false);
+pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+pref("browser.newtabpage.activity-stream.system.showSponsored", false);
+pref("browser.newtabpage.activity-stream.showSponsored", false);
+pref("extensions.pocket.enabled", false);
+pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+pref("network.dns.echconfig.enabled", true);
+pref("network.dns.http3_echconfig.enabled", true);
+pref("browser.toolbars.bookmarks.visibility", "always");
+pref("media.ffmpeg.vaapi.enabled",true);
+pref("media.ffvpx.enabled",false);
+pref("media.rdd-vpx.enabled",false);
+pref("media.navigator.mediadatadecoder_vpx_enabled",true);
+pref("image.webp",false);
+pref("browser.download.viewableInternally.typeWasRegistered.webp",false);
+pref("drm",true);
+EOF
+
+sudo \rm $DIR/distribution/policies.json 2> /dev/null
+sudo mkdir -p $DIR/distribution 2> /dev/null
+#about:memory -> measure
+sudo tee $DIR/distribution/policies.json >/dev/null <<'EOF'
+{
+  "policies": {
+    "ExtensionSettings": {
+      "uBlock0@raymondhill.net": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
+      },
+      "{26b743a8-b1b0-4b8c-a51e-0fc3797727a8}": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/google-consent-dialog-remover/latest.xpi"
+      },
+      "DontFuckWithPaste@raim.ist": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/don-t-fuck-with-paste/latest.xpi"
+      },
+      "jid1-MnnxcxisBPnSXQ@jetpack": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi"
+      },
+      "newtaboverride@agenedia.com": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/new-tab-override/latest.xpi"
+      },
+      "pure-url@jetpack": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/pure-url/latest.xpi"
+      },
+      "gdpr@cavi.au.dk": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/consent-o-matic/latest.xpi"
+      },
+      "magnolia_limited_permissions@12.34": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://gitflic.ru/project/magnolia1234/bpc_uploads/blob/raw?file=bypass_paywalls_clean-3.8.8.0-custom.xpi"
+      },
+      "{74145f27-f039-47ce-a470-a662b129930a}": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi"
+      },
+      "dont-track-me-google@robwu.nl": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/dont-track-me-google1/latest.xpi"
+      },
+      "gmailellcheckersimple@durasoft": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/gmail-checker-simple/latest.xpi"
+      },
+      "{abea9bb3-7bd0-48bc-88b1-39f0560744d6}": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/google-search-fixer-refreshed/latest.xpi"
+      },
+      "{00000f2a-7cde-4f20-83ed-434fcb420d71}": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/imagus/latest.xpi"
+      },
+      "support@netflux.me": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/netflux/latest.xpi"
+      },
+      "plasma-browser-integration@kde.org": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/plasma-integration/latest.xpi"
+      },
+      "chrome-gnome-shell@gnome.org": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/gnome-shell-integration/latest.xpi"
+      },
+      "{7c73b62b-7ac7-4292-81a7-c15746af0972}": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/google-search-display-icon/latest.xpi"
+      },
+      "{a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7}": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/user-agent-string-switcher/latest.xpi"
+      },
+      "NetflixPrime@Autoskip.io": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/netflix-prime-auto-skip/latest.xpi"
+      },
+      "78272b6fa58f4a1abaac99321d503a20@proton.me": {
+        "installation_mode": "normal_installed",
+        "install_url": "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi"
+      }
+    }
+  }
+}
+EOF
